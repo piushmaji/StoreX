3659af1801ac11119529510a2707e6a7d2fa065d
Piush Maji <piushmaji@gmail.com>
2026-02-02 22:56:16 +0530

    Update Address.jsx

--- src/components/Profile/Address.jsx
+++ src/components/Profile/Address.jsx
@@ -1,4 +1,4 @@
-import { Home, Pencil, Plus, Trash2 } from 'lucide-react'
+import { Home, MapPin, Pencil, Phone, Plus, Trash2 } from 'lucide-react'
 import React from 'react'
 
 const Address = () => {
@@ -24,21 +24,35 @@ const Address = () => {
                 <section className='grid grid-cols-2 gap-4 py-6'>
 
                     {/* each address section */}
-                    <div className='flex flex-col border rounded-lg border-gray-300 p-4'>
-                        <div className='flex justify-between'>
-                            <div className='flex gap-2'>
-                                <Home />
-                                <h1>Default Shipping Address</h1>
-                            </div>
+                    {[1, 2, 3, 4].map(() => (
+                        <div className='flex flex-col gap-4 border rounded-lg border-gray-300 shadow-lg p-4'>
+                            <div className='flex justify-between items-center'>
+                                <div className='flex gap-2 items-center'>
+                                    <Home size={16} />
+                                    <h1>Default Shipping Address</h1>
+                                </div>
 
-                            <div className='flex'>
-                                <div><Pencil /></div>
-                                <div><Trash2 /></div>
+                                <div className='flex gap-2'>
+                                    <div className='h-6 w-6 flex items-center justify-center rounded-md bg-blue-300 text-blue-600 p-1 hover:scale-95 transition-all'><Pencil size={16} /></div>
+                                    <div className='h-6 w-6 flex items-center justify-center rounded-md bg-red-300 text-red-600 p-1 hover:scale-95 transition-all'><Trash2 size={16} /></div>
+                                </div>
+                            </div>
+                            <div className='text-xl font-semibold px-6'>
+                                <h1>Piush Maji</h1>
+                            </div>
+                            <div className='flex flex-col gap-4'>
+                                <div className='flex gap-2 items-center text-xs'>
+                                    <MapPin size={16} />
+                                    <h1>Durga Puja Park,Karolbagh,Regharpura,New Delhi-110005</h1>
+                                </div>
+                                <div className='flex gap-2 items-center text-xs'>
+                                    <Phone size={16} />
+                                    <h1>9899076655</h1>
+                                </div>
                             </div>
                         </div>
-                        <div></div>
-                        <div></div>
-                    </div>
+                    ))}
+
                 </section>
             </div >
         </div >
