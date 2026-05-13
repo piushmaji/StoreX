aff844216a1f00768892b833036b33489fce04c8
Piush Maji <piushmaji@gmail.com>
2026-05-14 01:30:36 +0530

    Update priceHistoryService.js

--- src/services/priceHistoryService/priceHistoryService.js
+++ src/services/priceHistoryService/priceHistoryService.js
@@ -1,4 +1,4 @@
-import supabase from "../../lib/supabase/Supabase";
+import supabase from "../../lib/Supabase/Supabase";
 
 export async function getPriceHistory(productId) {
   const { data, error } = await supabase
